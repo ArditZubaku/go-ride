e4f756b6481225ba16f6f597e2992d4df2e5ab1e
Ardit Zubaku <zubaku92@gmail.com>
2025-11-09 20:34:00 +0100

    Implement graceful shutdown and error handling for HTTP server in trip service
    - Introduced a dedicated handleShutdown function to manage server termination and cleanup.
    - Enhanced main.go to support graceful shutdown on receiving interrupt signals.
    - Updated server initialization to log errors and handle shutdown gracefully.

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
+	"syscall"
+	"time"
 )
+
+const HTTP_ADDR = ":8083"
 
 func main() {
 	inMemRepo := repository.NewInMemRepository()
@@ -18,11 +25,40 @@ func main() {
 	mux.HandleFunc("POST /preview", httpHandler.HandleTripPreview)
 
 	server := &http.Server{
-		Addr:    ":8083",
+		Addr:    HTTP_ADDR,
 		Handler: mux,
 	}
+	serverErrorsChan := make(chan error, 1)
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("HTTP Server error: %v", err)
+	go func() {
+		log.Printf("Server listening on: %s", HTTP_ADDR)
+		serverErrorsChan <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	handleShutdown(server, serverErrorsChan, shutdown)
+}
+
+func handleShutdown(
+	server *http.Server,
+	serverErrorsChan chan error,
+	shutdown chan os.Signal,
+) {
+	select {
+	case err := <-serverErrorsChan:
+		log.Printf("Error starting the server: %v\n", err)
+	case sig := <-shutdown:
+		log.Printf("Server is shutting down due to: %v signal\n", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil { // Waits until all handlers finish or cancels based on the ctx
+			log.Printf("Could not shutdown the server gracefully because: %v\n", err)
+			if err := server.Close(); err != nil {
+				log.Printf("Could not close the server connections because: %v\n", err)
+			}
+		}
 	}
 }
