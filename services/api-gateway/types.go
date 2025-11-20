78f09456485deae8a8f71864030751a0e731b706
Ardit Zubaku <zubaku92@gmail.com>
2025-11-20 01:25:29 +0100

    Add previewTripRequest type and toProto method for trip service
    - Introduced a new previewTripRequest struct to encapsulate user trip details.
    - Implemented the toProto method to convert previewTripRequest into the corresponding Protocol Buffers format for trip preview requests.

--- services/api-gateway/types.go
+++ services/api-gateway/types.go
@@ -1,9 +1,26 @@
 package main
 
-import "ride-sharing/shared/types"
+import (
+	pb "ride-sharing/shared/proto/trip"
+	"ride-sharing/shared/types"
+)
 
 type previewTripRequest struct {
 	UserID      string            `json:"userID"`
 	PickUp      *types.Coordinate `json:"pickup"`
 	Destination *types.Coordinate `json:"destination"`
+}
+
+func (p *previewTripRequest) toProto() *pb.PreviewTripReq {
+	return &pb.PreviewTripReq{
+		UserID: p.UserID,
+		StartLocation: &pb.Coordinate{
+			Latitude:  p.PickUp.Latitude,
+			Longitude: p.PickUp.Longitude,
+		},
+		EndLocation: &pb.Coordinate{
+			Latitude:  p.Destination.Latitude,
+			Longitude: p.Destination.Longitude,
+		},
+	}
 }
