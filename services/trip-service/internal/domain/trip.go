3b1262364b3d6ba0954f4150638c281b06043951
Ardit Zubaku <zubaku92@gmail.com>
2025-11-23 01:16:23 +0100

    Add methods for ride fare management in TripService and TripRepository
    - Introduced SaveRideFare method in TripRepository for storing ride fare data.
    - Added EstimaPkgsPriceWithRoute and GenerateTripFares methods in TripService for enhanced fare estimation and generation functionalities.

--- services/trip-service/internal/domain/trip.go
+++ services/trip-service/internal/domain/trip.go
@@ -18,9 +18,12 @@ type TripModel struct {
 
 type TripRepository interface {
 	CreateTrip(ctx context.Context, trip *TripModel) (*TripModel, error)
+	SaveRideFare(ctx context.Context, fare *RideFareModel) error
 }
 
 type TripService interface {
 	CreateTrip(ctx context.Context, fare *RideFareModel) (*TripModel, error)
 	GetRoute(ctx context.Context, pickup, destination *types.Coordinate) (*tripTypes.OsrmAPIResponse, error)
+	EstimaPkgsPriceWithRoute(route *tripTypes.OsrmAPIResponse) []*RideFareModel
+	GenerateTripFares(ctx context.Context, fares []*RideFareModel, userID string) ([]*RideFareModel, error)
 }
